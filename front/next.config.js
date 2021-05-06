7d04320367bc44b82266bf591ee4cb74df76bfab
chu <akdlgusdnr26@gmail.com>
2021-05-06 12:53:42 +0900

    package update

--- front/next.config.js
+++ front/next.config.js
@@ -1,19 +1,19 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
-  });
+});
   
-  module.exports = withBundleAnalyzer({
-    compress: true,
-    webpack(config, { webpack }) {
-      const prod = process.env.NODE_ENV === 'production';
-      return {
-        ...config,
-        mode: prod ? 'production' : 'development',
-        devtool: prod ? 'hidden-source-map' : 'inline-source-map',
-        plugins: [
-          ...config.plugins,
-        ],
-      };
-    },
-  });
+module.exports = withBundleAnalyzer({
+  compress: true,
+  webpack(config, { webpack }) {
+    const prod = process.env.NODE_ENV === 'production';
+    return {
+      ...config,
+      mode: prod ? 'production' : 'development',
+      devtool: prod ? 'hidden-source-map' : 'inline-source-map',
+      plugins: [
+        ...config.plugins,
+      ],
+    };
+  },
+});
   

b9625b0236a58f2c35ab756ac0b690d6522c6b20
chu <akdlgusdnr26@gmail.com>
2022-05-06 17:14:46 +0900

    react-portfolio for aws

--- front/next.config.js
+++ front/next.config.js
@@ -4,6 +4,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   
 module.exports = withBundleAnalyzer({
   compress: true,
+  webpack5: false,
   webpack(config, { webpack }) {
     const prod = process.env.NODE_ENV === 'production';
     return {

a5f156a0308841c7a65f7c5d35246c1bbdccf1f4
chu <akdlgusdnr26@gmail.com>
2022-05-06 17:34:13 +0900

    code edit

--- front/next.config.js
+++ front/next.config.js
@@ -1,18 +1,18 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
+  enabled: process.env.ANALYZE === 'true',
 });
-  
+
 module.exports = withBundleAnalyzer({
   compress: true,
-  webpack5: false,
   webpack(config, { webpack }) {
     const prod = process.env.NODE_ENV === 'production';
     return {
       ...config,
       mode: prod ? 'production' : 'development',
-      devtool: prod ? 'hidden-source-map' : 'inline-source-map',
+      devtool: prod ? 'hidden-source-map' : 'eval-source-map',
       plugins: [
         ...config.plugins,
+        new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
       ],
     };
   },
